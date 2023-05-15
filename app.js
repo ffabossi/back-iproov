e137dfaeb89a96b9ce740d14eda24710b81933ae
Felipe Fabossi <felipefabossi@Jotfsans-MacBook-Pro.local>
2023-05-15 09:44:08 -0300

    FIRSST COMMIT

---
+++ app.js
@@ -0,0 +1,40 @@
+const axios = require('axios');
+const express = require('express');
+const bodyParser = require('body-parser');
+const ngrok = require('ngrok');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/enrol-token', async (req, res) => {
+  const url = 'https://us.rp.secure.iproov.me/api/v2/claim/enrol/token';
+  const body = req.body;
+
+  try {
+    const response = await axios.post(url, body);
+    res.send(response.data);
+    console.log(response.data)
+  } catch (error) {
+    console.error(error);
+    res.status(error.response.status).send(error.response.data);
+  }
+});
+
+const startServer = async () => {
+  const port = 3000;
+
+  try {
+    const url = await ngrok.connect({ addr: port });
+
+    console.log(`Server running on ${url}`);
+
+    app.listen(port, () => {
+      console.log(`App listening on port ${port}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+startServer();
+
