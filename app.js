400fb8659e9e95b707f297ac7b57d39b6324b15f
Felipe Fabossi <felipefabossi@Jotfsans-MacBook-Pro.local>
2023-05-15 10:24:52 -0300

    ngrok removed

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
-const axios = require('axios');
-const express = require('express');
-const bodyParser = require('body-parser');
-const ngrok = require('ngrok');
+const axios = require("axios");
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/enrol-token', async (req, res) => {
-  const url = 'https://us.rp.secure.iproov.me/api/v2/claim/enrol/token';
+app.post("/enrol-token", async (req, res) => {
+  const url = "https://us.rp.secure.iproov.me/api/v2/claim/enrol/token";
   const body = req.body;
 
   try {
     const response = await axios.post(url, body);
     res.send(response.data);
-    console.log(response.data)
+    console.log(response.data);
   } catch (error) {
     console.error(error);
     res.status(error.response.status).send(error.response.data);
-    
   }
 });
 
@@ -25,9 +23,7 @@ const startServer = async () => {
   const port = 3000;
 
   try {
-    const url = await ngrok.connect({ addr: port });
-
-    console.log(`Server running on ${url}`);
+    console.log(`Server running on ${port}`);
 
     app.listen(port, () => {
       console.log(`App listening on port ${port}`);
@@ -38,4 +34,3 @@ const startServer = async () => {
 };
 
 startServer();
-
