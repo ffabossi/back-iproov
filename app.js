4274c7bd01d473e6ba3381760a1d1731015c001a
Felipe Fabossi <felipefabossi@Jotfsans-MBP.localdomain>
2023-05-16 09:04:44 -0300

    get added

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const bodyParser = require("body-parser");
 
 const app = express();
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
 
 app.post("/enrol-token", async (req, res) => {
   const url = "https://us.rp.secure.iproov.me/api/v2/claim/enrol/token";
@@ -12,19 +17,16 @@ app.post("/enrol-token", async (req, res) => {
   try {
     const response = await axios.post(url, body);
     res.send(response.data);
-    console.log(response.data);
   } catch (error) {
     console.error(error);
     res.status(error.response.status).send(error.response.data);
   }
 });
 
-const startServer = async () => {
+const startServer = () => {
   const port = 3000;
-
   try {
     console.log(`Server running on ${port}`);
-
     app.listen(port, () => {
       console.log(`App listening on port ${port}`);
     });
